a309eac6b5e6d3fc687c4ea7583c2c42b3edebf6
Drew11 <drew.naumenko@gmail.com>
2020-09-24 15:55:29 +0300

    Updates

---
+++ precache-manifest.d9587e5d727a363fad0e35bdbf031186.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "243b3a81cbbadfaa650b159963165d32",
+    "url": "/werdevs-test-app/index.html"
+  },
+  {
+    "revision": "9cd2d646b0c43993e672",
+    "url": "/werdevs-test-app/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "1e78072a73fcaa0afb1e",
+    "url": "/werdevs-test-app/static/css/main.6cc72f33.chunk.css"
+  },
+  {
+    "revision": "9cd2d646b0c43993e672",
+    "url": "/werdevs-test-app/static/js/2.aecb3482.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/werdevs-test-app/static/js/2.aecb3482.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1e78072a73fcaa0afb1e",
+    "url": "/werdevs-test-app/static/js/main.07c26352.chunk.js"
+  },
+  {
+    "revision": "822f9bd39f4978b73588",
+    "url": "/werdevs-test-app/static/js/runtime-main.3d8ba652.js"
+  },
+  {
+    "revision": "b1a38376a7a592cc41e7b6701aada3d1",
+    "url": "/werdevs-test-app/static/media/header-logo.b1a38376.svg"
+  },
+  {
+    "revision": "37a75b54218416da18cdf1fa0650c569",
+    "url": "/werdevs-test-app/static/media/main-image.37a75b54.jpg"
+  }
+]);
